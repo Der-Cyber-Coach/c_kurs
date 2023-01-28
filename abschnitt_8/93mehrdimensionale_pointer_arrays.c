3b65dd42632e200b673e99f79a88fe84f3426e19
Simon Löschke <simon-loeschke@posteo.de>
2023-01-28 16:55:14 +0100

    weiter

--- abschnitt_8/93mehrdimensionale_pointer_arrays.c
+++ abschnitt_8/93mehrdimensionale_pointer_arrays.c
@@ -3,6 +3,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int *createArray(unsigned int length, int value)
+{
+    //nur Reservierung auf dem Heap-Speicher
+    int *array = (int *)malloc(length * sizeof(int));
+
+    if( array == NULL)
+    {
+        printf("Kein Speicher vorhanden, Abbruch!\n");
+        return NULL;
+    }
+
+    for (unsigned int i =0; i < length; i++)
+    {
+        array[i]=value;
+        printf("i = %d \t : ", i);
+        printf("array[%d] = %d\n",i,array[i]);
+    };
+    return array;
+}
+
+int *freeArray(int *array)
+{
+    if (array != NULL)
+    {
+    free(array);
+    };
+    return NULL;
+}
 
 int main ()
 {
