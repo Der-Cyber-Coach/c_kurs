5c2534ef1974b7ef3aab1eea9bc2662b52856473
Simon Löschke <simon-loeschke@posteo.de>
2023-01-28 16:42:17 +0100

    pointer array3

---
+++ abschnitt_8/91pointer_arrays3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    int a = 42;
+    int b =1337;
+    int c = -10;
+
+    int *array = (int *) malloc (3*sizeof(int));
+    array[o]=a;
+    array[1]=b;
+    array[2]=c;
+
+    free(array);
+    array = NULL;
+
+    return EXIT_SUCCESS;
+}
